05188b253b93485f8bb5a287172b5c813e46a875
inayat ullah <inayatullahkks@gmail.com>
2024-10-20 14:44:43 +0300

    fix

--- resources/views/mail/test.blade.php
+++ resources/views/mail/test.blade.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="ar" dir="rtl"> <!-- Setting the direction to RTL and language to Arabic -->
 <head>
-  
+    <meta charset="UTF-8">
+    <title>Email</title>
 </head>
 <body>
-    <h1> {{ $message }}</h1> <!-- Greeting added here -->
-  
-    
+    <h1>{{ $message }}</h1> <!-- The message will follow the RTL direction -->
 </body>
 </html>
